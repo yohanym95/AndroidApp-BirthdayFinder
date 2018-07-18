eaaf1ca4bb11d46f60e387c18183a9dd1aa4809b
yohanym95 <malshikay@gmail.com>
2018-07-18 19:13:35 +0530

    added home page

---
+++ app/src/main/java/com/example/yohan/newapp/HomePage.java
@@ -0,0 +1,46 @@
+package com.example.yohan.newapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
+
+
+public class HomePage extends AppCompatActivity {
+    Toolbar toolbar1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        toolbar1 = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu2,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String msg =" ";
+        switch (item.getItemId()){
+            case R.id.About:
+                msg = "About";
+                break;
+            case R.id.Exit:
+                System.exit(0);
+                break;
+        }
+
+        Toast.makeText(this,msg +" checked" ,Toast.LENGTH_LONG).show();
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
